13bcb97046a9c28c6db2ccba10b1cf1c2ebd898c
Laurens OB <laurensortiz@laurensob.local>
2020-12-01 01:39:35 -0600

    Updates

---
+++ precache-manifest.e0b6b66aa5b279e99112bcfead19bf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ef9af0bf620589d71753158d837fcb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "921f4471a0f15b84715a",
+    "url": "./static/css/main.5e3060a2.chunk.css"
+  },
+  {
+    "revision": "a26ed643d3774329d719",
+    "url": "./static/js/2.a0a74f8c.chunk.js"
+  },
+  {
+    "revision": "72b6ef66c85055d944e1d13796d32274",
+    "url": "./static/js/2.a0a74f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921f4471a0f15b84715a",
+    "url": "./static/js/main.9638329a.chunk.js"
+  },
+  {
+    "revision": "22cc7d09417b42ba7fcf",
+    "url": "./static/js/runtime-main.d91802df.js"
+  }
+]);
